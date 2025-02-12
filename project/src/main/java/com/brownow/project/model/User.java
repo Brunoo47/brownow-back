81c60aa27d42146d53e45ac3080470e6c714e14c
Brunoo47 <brunoostroskiprogramador@gmail.com>
2025-02-11 21:01:55 -0300

    Create repository test

--- project/src/main/java/com/brownow/project/model/User.java
+++ project/src/main/java/com/brownow/project/model/User.java
@@ -15,7 +15,7 @@ import java.util.Date;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Usuario {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
---
+++ project/src/main/java/com/brownow/project/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.brownow.project.repository;
+
+import com.brownow.project.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
+}
